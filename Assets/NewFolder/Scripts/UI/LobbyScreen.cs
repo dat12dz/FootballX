1064c0b50ccd81cb7f5536ac2a28018fec96d445
QuanHa203 <haquanbg203@gmail.com>
2023-09-30 23:47:57 +0700

    Edit LobbyScreen

--- Assets/NewFolder/Scripts/UI/LobbyScreen.cs
+++ Assets/NewFolder/Scripts/UI/LobbyScreen.cs
@@ -1,49 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class LobbyScreen : MonoBehaviour
 {
     private VisualElement root;
-    private VisualElement container;
-    private VisualElement redTeam;
-    private VisualElement blueTeam;
+    private static VisualElement container;
+    private static VisualElement lobbyScreen;
+    private static VisualElement background;
+    private static VisualElement redTeam;
+    private static VisualElement blueTeam;
+    private static VisualElement exitBtn;
 
-    private static VisualElement emptyPlayer;
-
-    private static VisualElement ownerPlayer;
-
-    private static VisualElement player;
-    private static VisualElement playerAvatar;
-    private static Label playerName;
     void Start()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
         container = root.Q<VisualElement>("container");
+        background = root.Q<VisualElement>("background");
+        lobbyScreen = root.Q<VisualElement>("lobby-screen");
         redTeam = root.Q<VisualElement>("red-team");
         blueTeam = root.Q<VisualElement>("blue-team");
-
+        exitBtn = root.Q<VisualElement>("exit-btn");
+        InitStyle();
     }
 
-    static void CreateEmptyPlayer()
+    private static void InitStyle()
     {
-
+        container.style.display = DisplayStyle.None;
+        lobbyScreen.style.display = DisplayStyle.None;
+        background.style.scale = new Scale(new Vector2(1.6f, 1.6f));
     }
 
-    static void CreatePlayer()
+    public static async void Show()
     {
-        
-    }
-
-    static void CreateOwnerPlayer()
-    {
-        
-    }
-
-    public static void Show()
-    {
-
+        container.style.display = DisplayStyle.Flex;
+        background.style.scale = new Scale(new Vector2(1f, 1f));
+        await Task.Delay(1000);
+        lobbyScreen.style.display = DisplayStyle.Flex;
+        exitBtn.RegisterCallback<PointerDownEvent>(callback =>
+        {
+            Debug.Log("click");
+            InitStyle();
+        });
     }
 }
