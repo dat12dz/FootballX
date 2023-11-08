2087d67caddf2035b2c01e13923980cfd0f56df8
QuanHa203 <haquanbg203@gmail.com>
2023-11-08 21:10:42 +0700

    commit

--- Assets/Script/UI/UInew_ShowFinalResult.cs
+++ Assets/Script/UI/UInew_ShowFinalResult.cs
@@ -1,4 +1,5 @@
 using Assets.Script.NetCode;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,52 +9,107 @@ using UnityEngine.UIElements;
 public class UInew_ShowFinalResult : MonoBehaviour
 {
     UIDocument document;
-    Label lb_OverallResult;
     VisualElement MvpDisplayer;
+    VisualElement OveralResult;
+    VisualElement ShowAllInfo;
+    VisualElement pnl_PlayerInfo;
+    VisualElement MatchResult_infoCard;
     Button btn_Continue1, btn_Continue2;
+    Label lb_OverallResult;
+    Label lb_PlayerName, lb_Score, lb_Change, lb_ShowKS;
     public static UInew_ShowFinalResult instance;
     private void Start()
     {
         var gameSystem = SceneHelper.GetGameSystem(gameObject.scene);
         instance = this;
+
         document = GetComponent<UIDocument>();
         var RootvsElement = document.rootVisualElement;
         MvpDisplayer = RootvsElement.Q<VisualElement>("pnl_MVPDisplayer");
+        OveralResult = RootvsElement.Q<VisualElement>("pnl_OveralResult");
+        ShowAllInfo = RootvsElement.Q<VisualElement>("pnl_ShowAllInfo");
+        pnl_PlayerInfo = RootvsElement.Q<VisualElement>("pnl_PlayerInfo");
+
         btn_Continue1 = RootvsElement.Q<Button>("Btn_continue1");
+        btn_Continue2 = RootvsElement.Q<Button>("Btn_continue2");
+
+        lb_OverallResult = RootvsElement.Q<Label>("lb_OverallResult");
+        lb_Score = RootvsElement.Q<Label>("lb_Score");
+        lb_Change = RootvsElement.Q<Label>("lb_Change");
+        lb_ShowKS = RootvsElement.Q<Label>("lb_ShowKS");
+
+        MatchResult_infoCard = Resources.Load<VisualTreeAsset>("/UITemplate/MatchResult_infoCard").CloneTree();
+        //lb_PlayerIndex = RootvsElement.Q<Label>("lb_PlayerIndex");
+        //lb_PlayerName = RootvsElement.Q<Label>("lb_PlayerName");
+        //lb_KS = RootvsElement.Q<Label>("lb_KS");
+        //lb_MVP = RootvsElement.Q<Label>("lb_MVP");
+
         btn_Continue1.clicked += () =>
         {
+            MvpDisplayer.style.display = DisplayStyle.None;
             DisplayAllInfomation(gameSystem.Client_GetAllPlayerList());
         };
-        lb_OverallResult = RootvsElement.Q<Label>("lb_OverallResult");
+
+        ResetStyle();
     }
+
     public async void ShowOverallMatchResult(string text,int sec)
     {
-        lb_OverallResult.style.display = DisplayStyle.Flex; 
-        lb_OverallResult.text = text;       
+        OveralResult.style.display = DisplayStyle.Flex;
+        lb_OverallResult.text = text;
         await Task.Delay(sec * 1000);
-        lb_OverallResult.style.display = DisplayStyle.None;
+        OveralResult.style.display = DisplayStyle.None;
     }
-    // Update is called once per frame
+
     public void DisplayMvp(Player mvpPlayer)
     {
         MvpDisplayer.style.display = DisplayStyle.Flex;
+
+        lb_PlayerName.text = mvpPlayer.initialPlayerData.Value.playerName.Value;
+        lb_Score.text = mvpPlayer.Score.ToString();
+
         if (mvpPlayer.isGoalKeeper)
         {
-            //  mvpPlayer.CatchedBallTimes.Value
-            //    mvpPlayer.Score
+            lb_Change.text = "Catch ball times";
+            lb_ShowKS.text = mvpPlayer.CatchedBallTimes.Value.ToString();
         }
+
         else
         {
-            //  mvpPlayer.TouchedBallTimes;
-            //  mvpPlayer.GoalTimes
+            lb_Change.text = "K/T";
+            lb_ShowKS.text = mvpPlayer.TouchedBallTimes.Value.ToString() + "/" + mvpPlayer.GoalTimes.Value.ToString();
         }
-    }    
+    }
     public void DisplayAllInfomation(Player[] allPlayerList)
     {
+        ShowAllInfo.style.display = DisplayStyle.Flex;
 
+        Label lb_PlayerIndex = MatchResult_infoCard.Q<Label>("lb_PlayerIndex");
+        Label lb_PlayerName = MatchResult_infoCard.Q<Label>("lb_PlayerName");
+        Label lb_KS = MatchResult_infoCard.Q<Label>("lb_KS");
+        Label lb_Score = MatchResult_infoCard.Q<Label>("lb_Score");
+        Label lb_MVP = MatchResult_infoCard.Q<Label>("lb_MVP");
+
+        allPlayerList = allPlayerList.OrderByDescending(p => p.Score).ToArray();
+        for (int i = 1; i <= allPlayerList.Length; i++)
+        {
+            lb_PlayerIndex.text = i.ToString();
+            lb_PlayerName.text = allPlayerList[i].name;
+            lb_KS.text = allPlayerList[i].Score.ToString();
+            lb_Score.text = allPlayerList[i].Score.ToString();
+
+            if(i > 2)
+            {
+                lb_MVP.text = null;
+            }
+            pnl_PlayerInfo.Add(MatchResult_infoCard);
+        }
     }
-    void Update()
+
+    void ResetStyle()
     {
-        
+        MvpDisplayer.style.display = DisplayStyle.None;
+        OveralResult.style.display = DisplayStyle.None;
+        ShowAllInfo.style.display = DisplayStyle.None;
     }
 }
--- Assets/Script/UINew/UINew_MVPScreen/UINew_MVPScreen.cs
+++ Assets/Script/UINew/UINew_MVPScreen/UINew_MVPScreen.cs
@@ -8,33 +8,65 @@ public class UINew_MVPScreen : MonoBehaviour
 {
     VisualElement root;
     static VisualElement container;
-    static VisualElement mvpScreenTop;
-    static VisualElement mvpScreenBottom;
+    static VisualElement mvpScreen;
+    static VisualElement firstBg;
+    static VisualElement secondBg;
+    static VisualElement thirdBg;
+    static VisualElement fourthBg;
+    static VisualElement widthScale;
+    static VisualElement fifthBg;
+
     Label playerName;
     void Start()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
         container = root.Q<VisualElement>("container");
-        mvpScreenTop = root.Q<VisualElement>("mvp-screen-top");
-        mvpScreenBottom = root.Q<VisualElement>("mvp-screen-bottom");
-        playerName = root.Q<Label>("player-name");
+        mvpScreen = root.Q<VisualElement>("mvp-screen");
+        firstBg = root.Q<VisualElement>("first-bg");
+        secondBg = root.Q<VisualElement>("second-bg");
+        thirdBg = root.Q<VisualElement>("third-bg");
+        fourthBg = root.Q<VisualElement>("fourth-bg");
+        widthScale = root.Q<VisualElement>("width-scale");
+        fifthBg = root.Q<VisualElement>("fifth-bg");
 
         ResetStyle();
     }
 
     public static void ResetStyle()
     {
-        mvpScreenTop.style.translate = new Translate(0, new Length(-110, LengthUnit.Percent));
-        mvpScreenBottom.style.translate = new Translate(0, new Length(110, LengthUnit.Percent));
+        mvpScreen.style.height = new Length(580, LengthUnit.Pixel);
+
+        firstBg.style.opacity = 0;
+        secondBg.style.opacity = 0;
+        thirdBg.style.opacity = 0;
+        thirdBg.style.unityBackgroundImageTintColor = new StyleColor(new Color(0,0,0));
+
+        fourthBg.style.translate = new Translate(new Length(-120, LengthUnit.Percent), 0);
+        widthScale.style.width = new Length(120, LengthUnit.Pixel);
+        fifthBg.style.width = new Length(0, LengthUnit.Pixel);
     }
 
     public static async void Show()
     {
-        mvpScreenTop.style.translate = new StyleTranslate(StyleKeyword.Null);
-        //await Task.Delay(400);
+        firstBg.style.opacity = 1;
+        secondBg.style.opacity = 1;
+        thirdBg.style.opacity = 1;
 
-        mvpScreenBottom.style.translate = new StyleTranslate(StyleKeyword.Null);
+        thirdBg.style.unityBackgroundImageTintColor = new StyleColor(new Color(0, 190f / 255, 240f / 255));
+
+        fourthBg.style.translate = new Translate(0, 0);
+        widthScale.style.width = new Length(0, LengthUnit.Pixel);
+        fifthBg.style.width = new Length(1200, LengthUnit.Pixel);
         await Task.Delay(400);
+
+        //await Task.Delay(100);
+        mvpScreen.style.height = new Length(400, LengthUnit.Pixel);
+
+        fourthBg.style.width = new Length(800, LengthUnit.Pixel);
+        await Task.Delay(400);
+        fourthBg.style.width = new Length(1200, LengthUnit.Pixel);
+
+        //fifthBg.style.width = new Length(0);
     }
 
 }
